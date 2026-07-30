2c319d0c0e94a8880d0d37ec3ab80e5e5ca518eb
Mahmoud <Sshss@example.comecho>
2026-07-30 23:52:52 +0300

    feat: implement AppInterceptors for authentication handling and FoodApiClient for food data API integration

--- lib/features/food/api/api_client/food_api_client.dart
+++ lib/features/food/api/api_client/food_api_client.dart
@@ -5,11 +5,25 @@ import 'package:retrofit/retrofit.dart';
 
 part 'food_api_client.g.dart';
 
+class FoodApiInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    options.baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+    super.onRequest(options, handler);
+  }
+}
+
 @lazySingleton
 @RestApi()
 abstract class FoodApiClient {
   @factoryMethod
-  factory FoodApiClient(Dio dio) = _FoodApiClient;
+  factory FoodApiClient(Dio dio) {
+    final foodDio = Dio(dio.options);
+    foodDio.interceptors.addAll(dio.interceptors);
+    foodDio.interceptors.add(FoodApiInterceptor());
+    
+    return _FoodApiClient(foodDio);
+  }
 
   @GET('/dummy')
   Future<void> dummy();
