12fe7f553318177827bdbcb372273368fbf1da64
Mahmoud <Sshss@example.comecho>
2026-07-31 08:12:12 +0300

    feat: implement FoodRemoteDataSource using FoodApiClient and injectable

--- lib/features/food/api/datasource/food_remote_data_source_impl.dart
+++ lib/features/food/api/datasource/food_remote_data_source_impl.dart
@@ -1,9 +1,9 @@
 import 'package:fitness/features/food/data/datasources/food_remote_data_source_contract.dart';
 import 'package:injectable/injectable.dart';
-
+import 'package:fitness/features/food/api/api_client/food_api_client.dart';
 @Injectable(as: FoodRemoteDataSourceContract)
 class FoodRemoteDataSourceImpl
     implements FoodRemoteDataSourceContract {
-  final FoodRemoteDataSourceImpl foodRemoteDataSourceImpl;
-  FoodRemoteDataSourceImpl({required this.foodRemoteDataSourceImpl});
+  final FoodApiClient foodApiClient;
+  FoodRemoteDataSourceImpl({required this.foodApiClient});
 }
